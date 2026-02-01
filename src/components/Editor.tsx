dd25742059642520f38ef6649ae993d05f751911
baba-yhuki <yuki66webdesign@gmail.com>
2026-02-01 16:09:00 +0900

    first commit

---
+++ src/components/Editor.tsx
@@ -0,0 +1,28 @@
+import { locales } from "@blocknote/core";
+import { useCreateBlockNote } from "@blocknote/react";
+import { ja } from "@blocknote/core/locales";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string | null;
+}
+
+function Editor({ onChange, initialContent }: EditorProps) {
+  const editor = useCreateBlockNote({
+    dictionary: ja,
+    initialContent:
+      initialContent != null ? JSON.parse(initialContent) : undefined,
+  });
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        onChange={() => onChange(JSON.stringify(editor.document))}
+      />
+    </div>
+  );
+}
+
+export default Editor;
